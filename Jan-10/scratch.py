f79d38ca250f29f5ba97d1d66e8aeaeafe2aa97f
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2023-01-12 19:14:17 -0600

    Add files from class

---
+++ Jan-10/scratch.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print(1+2)
+print(8/2)
+
+x = 7
+
+y = x     # here, y is x, which is 7
+x = 3     # here, x is 3
+print(y)  # here, y is ... ?
+
+#
+print("~~NEW EXAMPLE~~")
+# print(name, middle465654Name, last_name)
+name ="Melissa"
+last_name="Robbins"
+middle465654Name= "Lea"
+print(name, middle465654Name, last_name)
+
+
+lyric = "for whom the bell tolls -Metallica"
+
+
+
+
---
+++ Jan-12/test_file.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 19:08:30 2023
+
+@author: mrobbins
+"""
+
+print("Today was a good day")
+print("Yesterday, not so much")

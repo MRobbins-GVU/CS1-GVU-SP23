27b041e4c44e85dd669ce9c18a8b668b205cf7da
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2023-02-07 00:52:02 -0600

    Create question2.py
    scaffolding for question 2

---
+++ Exams/Exam1/question2.py
@@ -0,0 +1,20 @@
+# add evens
+
+def add_evens(start, stop):
+    # add up all even numbers between start and stop, not including either value.
+    total = 0
+    
+    # TODO: You will need to decide what goes in the range function, and how to add up the numbers
+    for x in range():
+        # TODO: You can check if a number is even using this formula: x % 2 == 0
+        
+    return total
+
+def main():
+    start_1 = 0
+    stop_1 = 10
+    ex_1 = add_evens(start_1, stop_1)
+    # TODO: You will need to modify this print statement
+    print("All evens between start and stop add up to value")
+    
+    # TODO: Test the second value pair of 3, 9
